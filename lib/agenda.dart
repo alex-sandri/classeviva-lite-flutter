6fae32a253963e4cc9f7aba8522af8e8d33ed478
Alex Sandri <me@alexsandri.com>
2020-08-05 18:19:26 +0200

    Add RefreshIndicator to the Agenda route

--- lib/agenda.dart
+++ lib/agenda.dart
@@ -3,107 +3,122 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 
-class Agenda extends StatelessWidget {
+class Agenda extends StatefulWidget {
+  @override
+  _AgendaState createState() => _AgendaState();
+}
+
+class _AgendaState extends State<Agenda> {
+  ClasseViva _session;
+
+  List<ClasseVivaAgendaItem> _items;
+
+  Future<void> _handleRefresh() async {
+    // TODO: Make the start end end dynamic
+    final List<ClasseVivaAgendaItem> items = await _session.getAgenda(DateTime(2020, 5, 1), DateTime(2020, 6, 1));
+
+    _items.sort((a, b) {
+      // Most recent first
+      return b.start.compareTo(a.start);
+    });
+
+    if (mounted)
+      setState(() {
+        _items = items;
+      });
+  }
+
+  void initState() {
+    super.initState();
+
+    SharedPreferences.getInstance().then((preferences) {
+      _session = ClasseViva(
+        sessionId: preferences.getString("sessionId"),
+        context: context
+      );
+
+      _handleRefresh();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: () async {
-        final SharedPreferences preferences = await SharedPreferences.getInstance();
-
-        return ClasseViva(
-          sessionId: preferences.getString("sessionId"),
-          context: context
-        );
-      }.call(),
-      builder: (context, AsyncSnapshot<ClasseViva> session) {
-        if (!session.hasData)
-          return Container(
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Agenda & Compiti'
+          ),
+          elevation: 0,
+        ),
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: Container(
             color: Theme.of(context).primaryColor,
-          );
+            width: double.infinity,
+            child: _session == null
+              ? Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                  ),
+                )
+              : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: _items == null
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                        ),
+                      )
+                    : ListView.separated(
+                        separatorBuilder: (context, index) => Divider(
+                          color: Theme.of(context).accentColor,
+                        ),
+                        shrinkWrap: true,
+                        itemCount: _items.length,
+                        itemBuilder: (context, index) {
+                          final ClasseVivaAgendaItem item = _items[index];
 
-        return Material(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                'Agenda & Compiti'
-              ),
-              elevation: 0,
-            ),
-            body: GestureDetector(
-              onTap: () {
-                FocusScope.of(context).requestFocus(FocusNode());
-              },
-              child: Container(
-                color: Theme.of(context).primaryColor,
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: FutureBuilder(
-                        future: session.data.getAgenda(DateTime(2020, 5, 1), DateTime(2020, 6, 1)),
-                        builder: (context, AsyncSnapshot<List<ClasseVivaAgendaItem>> agenda) {
-                          if (!agenda.hasData)
-                            return Center(
-                              child: CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                          return ListTile(
+                            title: SelectableText(
+                              item.autore_desc,
+                              style: TextStyle(
+                                color: Theme.of(context).accentColor,
+                                fontWeight: FontWeight.w900,
                               ),
-                            );
-
-                          agenda.data.sort((a, b) {
-                            // Most recent first
-                            return b.start.compareTo(a.start);
-                          });
-
-                          return ListView.separated(
-                            separatorBuilder: (context, index) => Divider(
-                              color: Theme.of(context).accentColor,
                             ),
-                            shrinkWrap: true,
-                            itemCount: agenda.data.length,
-                            itemBuilder: (context, index) {
-                              final ClasseVivaAgendaItem item = agenda.data[index];
-
-                              return ListTile(
-                                title: SelectableText(
-                                  item.autore_desc,
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(height: 5,),
+                                SelectableText(
+                                  "(${DateFormat.yMMMMd().add_jm().format(item.start)} - ${DateFormat.yMMMMd().add_jm().format(item.end)})",
                                   style: TextStyle(
                                     color: Theme.of(context).accentColor,
-                                    fontWeight: FontWeight.w900,
                                   ),
                                 ),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    SizedBox(height: 5,),
-                                    SelectableText(
-                                      "(${DateFormat.yMMMMd().add_jm().format(item.start)} - ${DateFormat.yMMMMd().add_jm().format(item.end)})",
-                                      style: TextStyle(
-                                        color: Theme.of(context).accentColor,
-                                      ),
-                                    ),
-                                    SizedBox(height: 5,),
-                                    SelectableText(
-                                      item.nota_2,
-                                      style: TextStyle(
-                                        color: Theme.of(context).accentColor,
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              );
-                            },
+                                SizedBox(height: 5,),
+                                SelectableText(
+                                  item.nota_2,
+                                  style: TextStyle(
+                                    color: Theme.of(context).accentColor,
+                                  ),
+                                ),
+                              ],
+                            )
                           );
                         },
                       ),
-                    ),
-                  ],
                 ),
-              ),
+              ],
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
