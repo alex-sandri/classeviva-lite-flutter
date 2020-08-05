e74deabdf2692f86d4ccdf1acdcfd19858bac92f
Alex Sandri <me@alexsandri.com>
2020-08-05 12:06:11 +0200

    Add RefreshIndicator in grades route

--- lib/grades.dart
+++ lib/grades.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 
-class Grades extends StatelessWidget {
+class Grades extends StatefulWidget {
+  @override
+  _GradesState createState() => _GradesState();
+}
+
+class _GradesState extends State<Grades> {
+  List<ClasseVivaGrade> _grades;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -20,6 +27,15 @@ class Grades extends StatelessWidget {
           return Container(
             color: Theme.of(context).primaryColor,
           );
+
+        Future<void> _handleRefresh() async
+        {
+          final List<ClasseVivaGrade> grades = await session.data.getGrades();
+
+          setState(() {
+            _grades = grades;
+          });
+        }
 
         return Material(
           child: Scaffold(
@@ -50,88 +66,91 @@ class Grades extends StatelessWidget {
                               ),
                             );
 
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: grades.data.length,
-                            itemBuilder: (context, index) {
-                              final ClasseVivaGrade grade = grades.data[index];
+                          return RefreshIndicator(
+                            onRefresh: _handleRefresh,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: grades.data.length,
+                              itemBuilder: (context, index) {
+                                final ClasseVivaGrade grade = grades.data[index];
 
-                              Color _getGradeColor(ClasseVivaGrade grade)
-                              {
-                                Color color;
-
-                                if (grade.type != "Voto Test")
+                                Color _getGradeColor(ClasseVivaGrade grade)
                                 {
-                                  double parsedGrade;
+                                  Color color;
 
-                                  if (grade.grade.contains("½")) parsedGrade = double.parse(grade.grade.replaceAll("½", ".5"));
-                                  else if (grade.grade.contains("+")) parsedGrade = double.parse(grade.grade.replaceAll("+", ".25"));
-                                  else if (grade.grade.contains("-")) parsedGrade = double.parse(grade.grade.replaceAll("-", ".75")) - 1;
+                                  if (grade.type != "Voto Test")
+                                  {
+                                    double parsedGrade;
 
-                                  if (parsedGrade == null) color = Colors.blue; // Letter instead of a number, TODO: parse grade letters
-                                  else if (parsedGrade >= 6) color = Colors.green;
-                                  else if (parsedGrade >= 5) color = Colors.orange;
-                                  else color = Colors.red;
+                                    if (grade.grade.contains("½")) parsedGrade = double.parse(grade.grade.replaceAll("½", ".5"));
+                                    else if (grade.grade.contains("+")) parsedGrade = double.parse(grade.grade.replaceAll("+", ".25"));
+                                    else if (grade.grade.contains("-")) parsedGrade = double.parse(grade.grade.replaceAll("-", ".75")) - 1;
+
+                                    if (parsedGrade == null) color = Colors.blue; // Letter instead of a number, TODO: parse grade letters
+                                    else if (parsedGrade >= 6) color = Colors.green;
+                                    else if (parsedGrade >= 5) color = Colors.orange;
+                                    else color = Colors.red;
+                                  }
+                                  else color = Colors.blue;
+
+                                  return color; 
                                 }
-                                else color = Colors.blue;
 
-                                return color; 
-                              }
-
-                              return ListTile(
-                                isThreeLine: true,
-                                leading: CircleAvatar(
-                                  child: Text(
-                                    grade.grade,
+                                return ListTile(
+                                  isThreeLine: true,
+                                  leading: CircleAvatar(
+                                    child: Text(
+                                      grade.grade,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    backgroundColor: _getGradeColor(grade),
+                                    radius: 25,
+                                  ),
+                                  title: Text(
+                                    grade.subject,
                                     style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
+                                      color: Theme.of(context).accentColor,
+                                      fontWeight: FontWeight.w900,
                                     ),
                                   ),
-                                  backgroundColor: _getGradeColor(grade),
-                                  radius: 25,
-                                ),
-                                title: Text(
-                                  grade.subject,
-                                  style: TextStyle(
-                                    color: Theme.of(context).accentColor,
-                                    fontWeight: FontWeight.w900,
-                                  ),
-                                ),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisSize: MainAxisSize.max,
-                                      children: <Widget>[
-                                        Text(
-                                          DateFormat.yMMMMd().format(grade.date),
-                                          style: TextStyle(
-                                            color: Theme.of(context).accentColor,
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: Text(
-                                            " - ${grade.type}".replaceAllMapped(RegExp(".{1}"), (match) => "\u{200b}${match.group(0)}"),
+                                  subtitle: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: <Widget>[
+                                          Text(
+                                            DateFormat.yMMMMd().format(grade.date),
                                             style: TextStyle(
                                               color: Theme.of(context).accentColor,
                                             ),
-                                            overflow: TextOverflow.ellipsis,
-                                            softWrap: false,
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    Text(
-                                      grade.description,
-                                      style: TextStyle(
-                                        color: Theme.of(context).accentColor,
+                                          Expanded(
+                                            child: Text(
+                                              " - ${grade.type}".replaceAllMapped(RegExp(".{1}"), (match) => "\u{200b}${match.group(0)}"),
+                                              style: TextStyle(
+                                                color: Theme.of(context).accentColor,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                              softWrap: false,
+                                            ),
+                                          ),
+                                        ],
                                       ),
-                                    ),
-                                  ],
-                                )
-                              );
-                            },
+                                      Text(
+                                        grade.description,
+                                        style: TextStyle(
+                                          color: Theme.of(context).accentColor,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                );
+                              },
+                            ),
                           );
                         },
                       ),
