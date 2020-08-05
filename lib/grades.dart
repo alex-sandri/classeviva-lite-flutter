5985ed1d580208ce48e4fb92bd7930000f0fc970
Alex Sandri <me@alexsandri.com>
2020-08-05 12:55:23 +0200

    Optimize the RefreshIndicator animation by removing the parent FutureBuilder

--- lib/grades.dart
+++ lib/grades.dart
@@ -56,104 +56,99 @@ class _GradesState extends State<Grades> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Expanded(
-                      child: FutureBuilder(
-                        future: _handleRefresh(),
-                        builder: (context, snap) {
-                          if (_grades == null)
-                            return Center(
-                              child: CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                      child: RefreshIndicator(
+                        onRefresh: _handleRefresh,
+                        color: Theme.of(context).primaryColor,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _grades?.length ?? 1,
+                          itemBuilder: (context, index) {
+                            if (_grades == null)
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                                ),
+                              );
+
+                            final ClasseVivaGrade grade = _grades[index];
+
+                            Color _getGradeColor(ClasseVivaGrade grade)
+                            {
+                              Color color;
+
+                              if (grade.type != "Voto Test")
+                              {
+                                double parsedGrade;
+
+                                if (grade.grade.contains("½")) parsedGrade = double.parse(grade.grade.replaceAll("½", ".5"));
+                                else if (grade.grade.contains("+")) parsedGrade = double.parse(grade.grade.replaceAll("+", ".25"));
+                                else if (grade.grade.contains("-")) parsedGrade = double.parse(grade.grade.replaceAll("-", ".75")) - 1;
+
+                                if (parsedGrade == null) color = Colors.blue; // Letter instead of a number, TODO: parse grade letters
+                                else if (parsedGrade >= 6) color = Colors.green;
+                                else if (parsedGrade >= 5) color = Colors.orange;
+                                else color = Colors.red;
+                              }
+                              else color = Colors.blue;
+
+                              return color; 
+                            }
+
+                            return ListTile(
+                              isThreeLine: true,
+                              leading: CircleAvatar(
+                                child: Text(
+                                  grade.grade,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                backgroundColor: _getGradeColor(grade),
+                                radius: 25,
                               ),
-                            );
-
-                          return RefreshIndicator(
-                            onRefresh: _handleRefresh,
-                            color: Theme.of(context).primaryColor,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: _grades.length,
-                              itemBuilder: (context, index) {
-                                final ClasseVivaGrade grade = _grades[index];
-
-                                Color _getGradeColor(ClasseVivaGrade grade)
-                                {
-                                  Color color;
-
-                                  if (grade.type != "Voto Test")
-                                  {
-                                    double parsedGrade;
-
-                                    if (grade.grade.contains("½")) parsedGrade = double.parse(grade.grade.replaceAll("½", ".5"));
-                                    else if (grade.grade.contains("+")) parsedGrade = double.parse(grade.grade.replaceAll("+", ".25"));
-                                    else if (grade.grade.contains("-")) parsedGrade = double.parse(grade.grade.replaceAll("-", ".75")) - 1;
-
-                                    if (parsedGrade == null) color = Colors.blue; // Letter instead of a number, TODO: parse grade letters
-                                    else if (parsedGrade >= 6) color = Colors.green;
-                                    else if (parsedGrade >= 5) color = Colors.orange;
-                                    else color = Colors.red;
-                                  }
-                                  else color = Colors.blue;
-
-                                  return color; 
-                                }
-
-                                return ListTile(
-                                  isThreeLine: true,
-                                  leading: CircleAvatar(
-                                    child: Text(
-                                      grade.grade,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                    backgroundColor: _getGradeColor(grade),
-                                    radius: 25,
-                                  ),
-                                  title: Text(
-                                    grade.subject,
-                                    style: TextStyle(
-                                      color: Theme.of(context).accentColor,
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  ),
-                                  subtitle: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                              title: Text(
+                                grade.subject,
+                                style: TextStyle(
+                                  color: Theme.of(context).accentColor,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisSize: MainAxisSize.max,
                                     children: <Widget>[
-                                      Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        children: <Widget>[
-                                          Text(
-                                            DateFormat.yMMMMd().format(grade.date),
-                                            style: TextStyle(
-                                              color: Theme.of(context).accentColor,
-                                            ),
-                                          ),
-                                          Expanded(
-                                            child: Text(
-                                              " - ${grade.type}".replaceAllMapped(RegExp(".{1}"), (match) => "\u{200b}${match.group(0)}"),
-                                              style: TextStyle(
-                                                color: Theme.of(context).accentColor,
-                                              ),
-                                              overflow: TextOverflow.ellipsis,
-                                              softWrap: false,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
                                       Text(
-                                        grade.description,
+                                        DateFormat.yMMMMd().format(grade.date),
                                         style: TextStyle(
                                           color: Theme.of(context).accentColor,
                                         ),
                                       ),
+                                      Expanded(
+                                        child: Text(
+                                          " - ${grade.type}".replaceAllMapped(RegExp(".{1}"), (match) => "\u{200b}${match.group(0)}"),
+                                          style: TextStyle(
+                                            color: Theme.of(context).accentColor,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                          softWrap: false,
+                                        ),
+                                      ),
                                     ],
-                                  )
-                                );
-                              },
-                            ),
-                          );
-                        },
+                                  ),
+                                  Text(
+                                    grade.description,
+                                    style: TextStyle(
+                                      color: Theme.of(context).accentColor,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            );
+                          },
+                        ),
                       ),
                     ),
                   ],
