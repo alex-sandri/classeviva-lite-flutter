08ffa56878061e83cdefa463dad6ce560ff63f22
Alex Sandri <me@alexsandri.com>
2020-08-07 16:26:03 +0200

    Use an ExpansionTile rather than a ListTile to show all grades for the clicked subject

--- lib/grades.dart
+++ lib/grades.dart
@@ -198,7 +198,7 @@ class _GradesState extends State<Grades> {
 
                       return Card(
                         color: Colors.transparent,
-                        child: ListTile(
+                        child: ExpansionTile(
                           leading: CircleAvatar(
                             backgroundColor: Colors.transparent,
                             child: Stack(
@@ -232,9 +232,76 @@ class _GradesState extends State<Grades> {
                               fontWeight: FontWeight.w900,
                             ),
                           ),
-                          onTap: () {
-                            // TODO
-                          },
+                          children: [
+                            ...grades.map((grade) {
+                              return ListTile(
+                                isThreeLine: true,
+                                leading: CircleAvatar(
+                                  child: Text(
+                                    grade.grade,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  backgroundColor: ClasseViva.getGradeColor(grade),
+                                  radius: 25,
+                                ),
+                                title: SelectableText(
+                                  grade.subject,
+                                  style: TextStyle(
+                                    color: Theme.of(context).accentColor,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: <Widget>[
+                                        SelectableText(
+                                          DateFormat.yMMMMd().format(grade.date),
+                                          style: TextStyle(
+                                            color: Theme.of(context).accentColor,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: SelectableText(
+                                            " - ${grade.type}",
+                                            style: TextStyle(
+                                              color: Theme.of(context).accentColor,
+                                            ),
+                                            maxLines: 1,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SelectableLinkify(
+                                      text: grade.description,
+                                      options: LinkifyOptions(humanize: false),
+                                      style: TextStyle(
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      onOpen: (link) async {
+                                        if (await canLaunch(link.url)) await launch(link.url);
+                                        else
+                                          showDialog(
+                                            context: context,
+                                            builder: (context) {
+                                              return AlertDialog(
+                                                title: Text("Errore"),
+                                                content: Text("Impossibile aprire il link"),
+                                              );
+                                            },
+                                          );
+                                      },
+                                    ),
+                                  ],
+                                )
+                              );
+                            }).toList(),
+                          ],
                         ),
                       );
                     },
