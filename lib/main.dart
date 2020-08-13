33c260901e89f418f9843eee1416e1a9442a6b57
Alex Sandri <me@alexsandri.com>
2020-08-13 16:18:18 +0200

    Remove cardTheme as the default one is better

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,6 @@ class MyApp extends StatelessWidget {
           color: Color(0xffcc1020),
           elevation: 0,
         ),
-        cardTheme: CardTheme(
-          color: Colors.transparent,
-        ),
         dividerTheme: DividerThemeData(
           color: Colors.black,
         ),
@@ -56,9 +53,6 @@ class MyApp extends StatelessWidget {
         appBarTheme: AppBarTheme(
           color: Color(0xffcc1020),
           elevation: 0,
-        ),
-        cardTheme: CardTheme(
-          color: Colors.transparent,
         ),
         dividerTheme: DividerThemeData(
           color: Colors.white,
