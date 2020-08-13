a2599ab4039d117d296b0f77ca5d0e9db71d953d
Alex Sandri <me@alexsandri.com>
2020-08-13 11:36:34 +0200

    Add theme setting

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  String _theme;
+
+  @override
+  void initState() {
+    super.initState();
+
+    SharedPreferences.getInstance().then((preferences) {
+      final String theme = preferences.getString("theme") ?? "system";
+
+      setState(() {
+        _theme = theme;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -11,8 +31,40 @@ class Settings extends StatelessWidget {
             'Impostazioni'
           ),
         ),
-        body: Container(
-          
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text(
+                "Tema",
+              ),
+              trailing: DropdownButton(
+                value: _theme,
+                items: [
+                  DropdownMenuItem(
+                    value: "system",
+                    child: Text("Predefinito"),
+                  ),
+                  DropdownMenuItem(
+                    value: "light",
+                    child: Text("Chiaro"),
+                  ),
+                  DropdownMenuItem(
+                    value: "dark",
+                    child: Text("Scuro"),
+                  ),
+                ],
+                onChanged: (value) async {
+                  final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+                  await preferences.setString("theme", value);
+
+                  setState(() {
+                    _theme = value;
+                  });
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
