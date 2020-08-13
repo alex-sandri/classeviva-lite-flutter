ac53985e4ccb21fbab1ad43c0b5084cc920b1fc3
Alex Sandri <me@alexsandri.com>
2020-08-13 16:00:58 +0200

    Fix: background color should be white with light theme

--- lib/absences.dart
+++ lib/absences.dart
@@ -1,4 +1,5 @@
 import 'package:classeviva_lite/classeviva.dart';
+import 'package:classeviva_lite/widgets/spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -52,112 +53,83 @@ class _AbsencesState extends State<Absences> {
           onTap: () {
             FocusScope.of(context).requestFocus(FocusNode());
           },
-          child: Container(
-            color: Theme.of(context).brightness == Brightness.light
-              ? Theme.of(context).primaryColor
-              : null,
-            width: double.infinity,
-            child: _session == null
-              ? Center(
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
-                  ),
-                )
-              : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: RefreshIndicator(
-                    onRefresh: _handleRefresh,
-                    color: Theme.of(context).primaryColor,
-                    child: _absences == null
-                    ? Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
-                        ),
-                      )
-                    : ListView.separated(
-                        separatorBuilder: (context, index) => Divider(
-                          color: Theme.of(context).accentColor,
-                        ),
-                        itemCount: _absences.length + 1,
-                        itemBuilder: (context, index) {
-                          if (_absences.isEmpty)
-                            return SelectableText(
-                              "Non sono presenti assenze o ritardi",
-                              style: TextStyle(
-                                color: Theme.of(context).accentColor,
-                              ),
-                              textAlign: TextAlign.center,
-                            );
-
-                          if (index == _absences.length) return Container();
-
-                          final ClasseVivaAbsence absence = _absences[index];
-
-                          String type;
-
-                          switch (absence.type)
-                          {
-                            case ClasseVivaAbsenceType.Absence: type = "Assenza"; break;
-                            case ClasseVivaAbsenceType.Late: type = "Ritardo"; break;
-                            case ClasseVivaAbsenceType.ShortDelay: type = "Ritardo Breve"; break;
-                            case ClasseVivaAbsenceType.EarlyExit: type = "Uscita Anticipata"; break;
-                          }
-
-                          String status;
-
-                          switch (absence.status)
-                          {
-                            case ClasseVivaAbsenceStatus.Justified: status = "Giustificata"; break;
-                            case ClasseVivaAbsenceStatus.NotJustified: status = "Non Giustificata"; break;
-                          }
-
-                          return ListTile(
-                            title: SelectableText(
-                              type,
-                              style: TextStyle(
-                                color: Theme.of(context).accentColor,
-                                fontWeight: FontWeight.w900,
-                              ),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                SizedBox(height: 5,),
-                                SelectableText(
-                                  absence.from == absence.to
-                                    ? DateFormat.yMMMMd().format(absence.from)
-                                    : "${DateFormat.yMMMMd().format(absence.from)} - ${DateFormat.yMMMMd().format(absence.to)}",
-                                  style: TextStyle(
-                                    color: Theme.of(context).accentColor,
-                                  ),
-                                ),
-                                SizedBox(height: 5,),
-                                SelectableText(
-                                  status,
-                                  style: TextStyle(
-                                    color: Theme.of(context).accentColor,
-                                  ),
-                                ),
-                                SizedBox(height: 5,),
-
-                                if (absence.description.isNotEmpty)
-                                  SelectableText(
-                                    absence.description,
-                                    style: TextStyle(
-                                      color: Theme.of(context).accentColor,
-                                    ),
-                                  ),
-                              ],
-                            )
+          child: _session == null
+            ? Spinner()
+            : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: _handleRefresh,
+                  color: Theme.of(context).primaryColor,
+                  child: _absences == null
+                  ? Spinner()
+                  : ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: _absences.length + 1,
+                      itemBuilder: (context, index) {
+                        if (_absences.isEmpty)
+                          return SelectableText(
+                            "Non sono presenti assenze o ritardi",
+                            textAlign: TextAlign.center,
                           );
-                        },
-                      ),
-                  )
-                ),
-              ],
-            ),
+
+                        if (index == _absences.length) return Container();
+
+                        final ClasseVivaAbsence absence = _absences[index];
+
+                        String type;
+
+                        switch (absence.type)
+                        {
+                          case ClasseVivaAbsenceType.Absence: type = "Assenza"; break;
+                          case ClasseVivaAbsenceType.Late: type = "Ritardo"; break;
+                          case ClasseVivaAbsenceType.ShortDelay: type = "Ritardo Breve"; break;
+                          case ClasseVivaAbsenceType.EarlyExit: type = "Uscita Anticipata"; break;
+                        }
+
+                        String status;
+
+                        switch (absence.status)
+                        {
+                          case ClasseVivaAbsenceStatus.Justified: status = "Giustificata"; break;
+                          case ClasseVivaAbsenceStatus.NotJustified: status = "Non Giustificata"; break;
+                        }
+
+                        return ListTile(
+                          title: SelectableText(
+                            type,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(height: 5,),
+                              SelectableText(
+                                absence.from == absence.to
+                                  ? DateFormat.yMMMMd().format(absence.from)
+                                  : "${DateFormat.yMMMMd().format(absence.from)} - ${DateFormat.yMMMMd().format(absence.to)}",
+                              ),
+                              SizedBox(height: 5,),
+                              SelectableText(
+                                status,
+                              ),
+                              SizedBox(height: 5,),
+
+                              if (absence.description.isNotEmpty)
+                                SelectableText(
+                                  absence.description,
+                                ),
+                            ],
+                          )
+                        );
+                      },
+                    ),
+                )
+              ),
+            ],
           ),
         ),
       ),
