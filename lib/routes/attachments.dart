94cdf7fa6be9c1d3642d71f5c429de057e2041c5
Alex Sandri <me@alexsandri.com>
2020-08-15 09:51:37 +0200

    Fix: attachment icon's background should be the same as the app bar color

--- lib/routes/attachments.dart
+++ lib/routes/attachments.dart
@@ -255,7 +255,7 @@ class _AttachmentsState extends State<Attachments> {
                                       _getAttachmentIcon(attachment),
                                       color: Theme.of(context).accentColor,
                                     ),
-                                    backgroundColor: Theme.of(context).primaryColor,
+                                    backgroundColor: Theme.of(context).appBarTheme.color,
                                     radius: 25,
                                   ),
                                   title: Text(
