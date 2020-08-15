73e77fc0b63c425ea853598bbc162638afaf0188
Alex Sandri <me@alexsandri.com>
2020-08-15 09:42:08 +0200

    Remove unused imports

--- lib/routes/calendar.dart
+++ lib/routes/calendar.dart
@@ -1,10 +1,8 @@
 import 'package:classeviva_lite/classeviva.dart';
 import 'package:classeviva_lite/routes/agenda.dart';
 import 'package:classeviva_lite/routes/grades.dart';
-import 'package:classeviva_lite/theme_manager.dart';
 import 'package:classeviva_lite/widgets/spinner.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class Calendar extends StatefulWidget {
   @override
