f9d7c805a1d3bab2a442a427ff79e2aa5f06bd63
Alex Sandri <me@alexsandri.com>
2020-09-01 09:23:15 +0200

    Remove useless widgets

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -51,186 +51,177 @@ class _HomeState extends State<Home> {
               onTap: () {
                 FocusScope.of(context).requestFocus(FocusNode());
               },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: _showLoadingSpinner
-                  ? [
-                      Expanded(
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              child: _showLoadingSpinner
+                ?
+                  Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
+                  )
+                :
+                  ListView(
+                    padding: EdgeInsets.all(15),
+                    children: <Widget>[
+                      FutureBuilder(
+                        future: session.data.getProfile(),
+                        builder: (context, AsyncSnapshot<ClasseVivaProfile> profile) {
+                          if (!profile.hasData)
+                            return SkeletonAnimation(
+                              shimmerColor: Colors.white54,
+                              gradientColor: Color.fromARGB(0, 244, 244, 244),
+                              curve: Curves.fastOutSlowIn,
+                              child: Container(  
+                                width: double.infinity,  
+                                height: 57, // Horrible but it works (height of the column below)
+                                decoration: BoxDecoration(  
+                                  color: Theme.of(context).disabledColor,  
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            );
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SelectableText(
+                                profile.data.name,
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              SelectableText(
+                                profile.data.school,
+                              ),
+
+                              if (session.data.getShortYear() != "")
+                                SelectableText(
+                                  "20${session.data.getShortYear()}/20${int.parse(session.data.getShortYear()) + 1}",
+                                ),
+                            ],
+                          );
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.today,
                           ),
+                          title: Text(
+                            "Registro",
+                          ),
+                          onTap: () => Get.to(Calendar()),
                         ),
                       ),
-                    ]
-                  : <Widget>[
-                      Expanded(
-                        child: ListView(
-                          padding: EdgeInsets.all(15),
-                          children: <Widget>[
-                            FutureBuilder(
-                              future: session.data.getProfile(),
-                              builder: (context, AsyncSnapshot<ClasseVivaProfile> profile) {
-                                if (!profile.hasData)
-                                  return SkeletonAnimation(
-                                    shimmerColor: Colors.white54,
-                                    gradientColor: Color.fromARGB(0, 244, 244, 244),
-                                    curve: Curves.fastOutSlowIn,
-                                    child: Container(  
-                                      width: double.infinity,  
-                                      height: 57, // Horrible but it works (height of the column below)
-                                      decoration: BoxDecoration(  
-                                        color: Theme.of(context).disabledColor,  
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                    ),
-                                  );
-
-                                return Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    SelectableText(
-                                      profile.data.name,
-                                      style: TextStyle(
-                                        fontSize: 35,
-                                        fontWeight: FontWeight.w900,
-                                      ),
-                                    ),
-                                    SelectableText(
-                                      profile.data.school,
-                                    ),
-
-                                    if (session.data.getShortYear() != "")
-                                      SelectableText(
-                                        "20${session.data.getShortYear()}/20${int.parse(session.data.getShortYear()) + 1}",
-                                      ),
-                                  ],
-                                );
-                              },
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.today,
-                                ),
-                                title: Text(
-                                  "Registro",
-                                ),
-                                onTap: () => Get.to(Calendar()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.grade,
-                                ),
-                                title: Text(
-                                  "Valutazioni",
-                                ),
-                                onTap: () => Get.to(Grades()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.watch_later,
-                                ),
-                                title: Text(
-                                  "Assenze / Ritardi",
-                                ),
-                                onTap: () => Get.to(Absences()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.bookmark_border,
-                                ),
-                                title: Text(
-                                  "Bacheca",
-                                ),
-                                onTap: () => Get.to(BulletinBoard()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.book,
-                                ),
-                                title: Text(
-                                  "Lezioni",
-                                ),
-                                onTap: () => Get.to(Lessons()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.view_agenda,
-                                ),
-                                title: Text(
-                                  "Agenda & Compiti",
-                                ),
-                                onTap: () => Get.to(Agenda()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.attachment,
-                                ),
-                                title: Text(
-                                  "Didattica",
-                                ),
-                                onTap: () => Get.to(Attachments()),
-                              ),
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.note,
-                                ),
-                                title: Text(
-                                  "Note",
-                                ),
-                                onTap: () => Get.to(Demerits()),
-                              ),
-                            ),
-                            Divider(
-                              color: Theme.of(context).accentColor,
-                              thickness: 2,
-                              indent: 4,
-                              endIndent: 4,
-                            ),
-                            Card(
-                              child: ListTile(
-                                leading: Icon(
-                                  Icons.skip_previous,
-                                ),
-                                title: Text(
-                                  "Anno Precedente",
-                                ),
-                                onTap: () async {
-                                  setState(() {
-                                    _showLoadingSpinner = true;
-                                  });
-
-                                  await ClasseViva.createSession(
-                                    session.data.session.uid,
-                                    session.data.session.pwd,
-                                    year: (int.parse(session.data.getShortYear(false)) - 1).toString(),
-                                  );
-
-                                  Get.offAll(Home());
-                                },
-                              ),
-                            ),
-                          ],
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.grade,
+                          ),
+                          title: Text(
+                            "Valutazioni",
+                          ),
+                          onTap: () => Get.to(Grades()),
                         ),
-                      )
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.watch_later,
+                          ),
+                          title: Text(
+                            "Assenze / Ritardi",
+                          ),
+                          onTap: () => Get.to(Absences()),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.bookmark_border,
+                          ),
+                          title: Text(
+                            "Bacheca",
+                          ),
+                          onTap: () => Get.to(BulletinBoard()),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.book,
+                          ),
+                          title: Text(
+                            "Lezioni",
+                          ),
+                          onTap: () => Get.to(Lessons()),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.view_agenda,
+                          ),
+                          title: Text(
+                            "Agenda & Compiti",
+                          ),
+                          onTap: () => Get.to(Agenda()),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.attachment,
+                          ),
+                          title: Text(
+                            "Didattica",
+                          ),
+                          onTap: () => Get.to(Attachments()),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.note,
+                          ),
+                          title: Text(
+                            "Note",
+                          ),
+                          onTap: () => Get.to(Demerits()),
+                        ),
+                      ),
+                      Divider(
+                        color: Theme.of(context).accentColor,
+                        thickness: 2,
+                        indent: 4,
+                        endIndent: 4,
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.skip_previous,
+                          ),
+                          title: Text(
+                            "Anno Precedente",
+                          ),
+                          onTap: () async {
+                            setState(() {
+                              _showLoadingSpinner = true;
+                            });
+
+                            await ClasseViva.createSession(
+                              session.data.session.uid,
+                              session.data.session.pwd,
+                              year: (int.parse(session.data.getShortYear(false)) - 1).toString(),
+                            );
+
+                            Get.offAll(Home());
+                          },
+                        ),
+                      ),
                     ],
               ),
             ),
