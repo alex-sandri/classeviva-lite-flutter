64cad98219f35f8cdf9ac64d7ce4b00d4f380331
Alex Sandri <me@alexsandri.com>
2020-09-02 17:57:20 +0200

    Create Book route

---
+++ lib/routes/book.dart
@@ -0,0 +1,74 @@
+import 'package:classeviva_lite/classeviva.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Book extends StatefulWidget {
+  final ClasseVivaBook item;
+
+  Book(this.item);
+
+  @override
+  _BookState createState() => _BookState();
+}
+
+class _BookState extends State<Book> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              widget.item.title,
+            ),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Info",
+                  icon: Icon(Icons.info_outline),
+                ),
+                Tab(
+                  text: "TODO",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ListView(
+                padding: EdgeInsets.all(8),
+                children: [
+                  Wrap(
+                    spacing: 4,
+                    children: widget.item.categories.map((category) {
+                      return Chip(
+                        label: Text(
+                          category,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  SelectableText(
+                    widget.item.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  SelectableText(
+                    widget.item.description,
+                  ),
+                ],
+              ),
+              Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
