cc2cb6ee9de6a054a5db3d40cdfd10e4ad516f60
Alex Sandri <me@alexsandri.com>
2020-09-06 13:19:46 +0200

    Column is useless

--- lib/widgets/sessions_list.dart
+++ lib/widgets/sessions_list.dart
@@ -33,110 +33,105 @@ class _SessionsListState extends State<SessionsList> {
       shrinkWrap: widget.shrinkWrap,
       padding: const EdgeInsets.all(15),
       children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              child: FlatButton(
-                color: ThemeManager.isLightTheme(context)
-                  ? Theme.of(context).primaryColor
-                  : Theme.of(context).accentColor,
-                colorBrightness: ThemeManager.isLightTheme(context)
-                  ? Brightness.dark
-                  : Brightness.light,
-                padding: EdgeInsets.all(15),
-                child: Text(
-                  "Esci da tutte le sessioni"
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                ),
-                onPressed: () async {
-                  for (int i = 0; i < _sessions.length; i++)
-                    await _sessions[i].signOut();
-
-                  Get.offAll(SignIn());
-                },
-              ),
+        Container(
+          width: double.infinity,
+          child: FlatButton(
+            color: ThemeManager.isLightTheme(context)
+              ? Theme.of(context).primaryColor
+              : Theme.of(context).accentColor,
+            colorBrightness: ThemeManager.isLightTheme(context)
+              ? Brightness.dark
+              : Brightness.light,
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Esci da tutte le sessioni"
             ),
-            SizedBox(
-              height: 15,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
             ),
-            ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: _sessions.length,
-              itemBuilder: (context, index) {
-                final ClasseViva session = ClasseViva(_sessions[index]);
+            onPressed: () async {
+              for (int i = 0; i < _sessions.length; i++)
+                await _sessions[i].signOut();
 
-                return FutureBuilder<ClasseVivaProfile>(
-                  future: session.getProfile(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData)
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: SkeletonAnimation(
-                          shimmerColor: Colors.white54,
-                          gradientColor: Color.fromARGB(0, 244, 244, 244),
-                          curve: Curves.fastOutSlowIn,
-                          child: Container(  
-                            width: double.infinity,  
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).disabledColor,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                        ),
-                      );
+              Get.offAll(SignIn());
+            },
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: _sessions.length,
+          itemBuilder: (context, index) {
+            final ClasseViva session = ClasseViva(_sessions[index]);
 
-                    return Card(
-                      margin: EdgeInsets.symmetric(vertical: 4),
-                      child: Dismissible(
-                        key: ValueKey(_sessions[index]),
-                        onDismissed: (direction) {
-                          final bool isCurrentSession = ClasseViva.isSignedIn() && ClasseViva.getCurrentSession().id == _sessions[index].id;
-
-                          _sessions[index].signOut();
-
-                          _sessions.removeAt(index);
-
-                          Scaffold
-                            .of(context)
-                            .showSnackBar(SnackBar(content: Text("Sessione rimossa")));
-
-                          if (isCurrentSession) Get.offAll(SignIn());
-                          else setState(() {});
-                        },
-                        background: Container(
-                          color: Colors.red,
-                          child: Icon(
-                            Icons.exit_to_app,
-                            color: Colors.white,
-                          ),
-                        ),
-                        child: ListTile(
-                          title: Text(
-                            snapshot.data.name,
-                          ),
-                          subtitle: Text(
-                            "${snapshot.data.school} (${session.getYear()}/${session.getYear() + 1})",
-                          ),
-                          leading: snapshot.data.avatar,
-                          onTap: () {
-                            ClasseViva.setCurrentSession(_sessions[index]);
-
-                            Get.offAll(Home());
-                          },
+            return FutureBuilder<ClasseVivaProfile>(
+              future: session.getProfile(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: SkeletonAnimation(
+                      shimmerColor: Colors.white54,
+                      gradientColor: Color.fromARGB(0, 244, 244, 244),
+                      curve: Curves.fastOutSlowIn,
+                      child: Container(  
+                        width: double.infinity,  
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).disabledColor,
+                          borderRadius: BorderRadius.circular(5),
                         ),
                       ),
-                    );
-                  }
+                    ),
+                  );
+
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 4),
+                  child: Dismissible(
+                    key: ValueKey(_sessions[index]),
+                    onDismissed: (direction) {
+                      final bool isCurrentSession = ClasseViva.isSignedIn() && ClasseViva.getCurrentSession().id == _sessions[index].id;
+
+                      _sessions[index].signOut();
+
+                      _sessions.removeAt(index);
+
+                      Scaffold
+                        .of(context)
+                        .showSnackBar(SnackBar(content: Text("Sessione rimossa")));
+
+                      if (isCurrentSession) Get.offAll(SignIn());
+                      else setState(() {});
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      child: Icon(
+                        Icons.exit_to_app,
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        snapshot.data.name,
+                      ),
+                      subtitle: Text(
+                        "${snapshot.data.school} (${session.getYear()}/${session.getYear() + 1})",
+                      ),
+                      leading: snapshot.data.avatar,
+                      onTap: () {
+                        ClasseViva.setCurrentSession(_sessions[index]);
+
+                        Get.offAll(Home());
+                      },
+                    ),
+                  ),
                 );
-              },
-            ),
-          ],
+              }
+            );
+          },
         )
       ],
     );
