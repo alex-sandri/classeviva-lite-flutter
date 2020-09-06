a01beee2126bc07bf9f3604d3a0f14fed119ae04
Alex Sandri <me@alexsandri.com>
2020-09-06 11:56:43 +0200

    Use SessionsList widget in SignIn

--- lib/routes/sign_in.dart
+++ lib/routes/sign_in.dart
@@ -1,10 +1,10 @@
 import 'package:classeviva_lite/classeviva.dart';
 import 'package:classeviva_lite/routes/home.dart';
 import 'package:classeviva_lite/theme_manager.dart';
+import 'package:classeviva_lite/widgets/sessions_list.dart';
 import 'package:classeviva_lite/widgets/spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:skeleton_text/skeleton_text.dart';
 
 class SignIn extends StatefulWidget {
   final bool showAccountChooser;
@@ -165,137 +165,7 @@ class _SignInState extends State<SignIn> {
               ),
 
               if (widget.showAccountChooser)
-                FutureBuilder<List<ClasseVivaSession>>(
-                  future: ClasseViva.getAllSessions(),
-                  builder: (context, sessions) {
-                    if (!sessions.hasData || sessions.data.isEmpty) return Container();
-
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Scegli un account',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Container(
-                          width: double.infinity,
-                          child: FlatButton(
-                            color: ThemeManager.isLightTheme(context)
-                              ? Theme.of(context).primaryColor
-                              : Theme.of(context).accentColor,
-                            colorBrightness: ThemeManager.isLightTheme(context)
-                              ? Brightness.dark
-                              : Brightness.light,
-                            padding: EdgeInsets.all(15),
-                            child: Text(
-                              "Esci da tutte le sessioni"
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(4)),
-                            ),
-                            onPressed: () async {
-                              for (int i = 0; i < sessions.data.length; i++)
-                                await sessions.data[i].signOut();
-
-                              Get.offAll(SignIn());
-                            },
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        ListView.builder(
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: sessions.data.length,
-                          itemBuilder: (context, index) {
-                            final ClasseViva session = ClasseViva(sessions.data[index]);
-
-                            return FutureBuilder<ClasseVivaBasicProfile>(
-                              future: session.getBasicProfile(),
-                              builder: (context, snapshot) {
-                                if (!snapshot.hasData)
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 4),
-                                    child: SkeletonAnimation(
-                                      shimmerColor: Colors.white54,
-                                      gradientColor: Color.fromARGB(0, 244, 244, 244),
-                                      curve: Curves.fastOutSlowIn,
-                                      child: Container(  
-                                        width: double.infinity,  
-                                        height: 50,
-                                        decoration: BoxDecoration(
-                                          color: Theme.of(context).disabledColor,
-                                          borderRadius: BorderRadius.circular(5),
-                                        ),
-                                      ),
-                                    ),
-                                  );
-
-                                return Card(
-                                  margin: EdgeInsets.symmetric(vertical: 4),
-                                  child: Dismissible(
-                                    key: ValueKey(sessions.data[index]),
-                                    onDismissed: (direction) async {
-                                      final bool isCurrentSession = await ClasseViva.isSignedIn() && (await ClasseViva.getCurrentSession()).id == sessions.data[index].id;
-
-                                      await sessions.data[index].signOut();
-
-                                      sessions.data.removeAt(index);
-
-                                      Scaffold
-                                        .of(context)
-                                        .showSnackBar(SnackBar(content: Text("Sessione rimossa")));
-
-                                      if (isCurrentSession) Get.offAll(SignIn());
-                                      else setState(() {});
-                                    },
-                                    background: Container(
-                                      color: Colors.red,
-                                      child: Icon(
-                                        Icons.exit_to_app,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    child: ListTile(
-                                      title: Text(
-                                        snapshot.data.name,
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w900,
-                                        ),
-                                      ),
-                                      subtitle: Text(
-                                        "${snapshot.data.school} (${session.getYear()}/${session.getYear() + 1})",
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                      onTap: () {
-                                        ClasseViva.setCurrentSession(sessions.data[index]);
-
-                                        _redirectToHomePage();
-                                      },
-                                    ),
-                                  ),
-                                );
-                              }
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                ),
+                SessionsList(),
             ],
           ),
         ),
