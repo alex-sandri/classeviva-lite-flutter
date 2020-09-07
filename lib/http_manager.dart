94123d79e52aa5f34439049189972278c96ae34e
Alex Sandri <me@alexsandri.com>
2020-09-07 19:24:19 +0200

    Create HttpManager class

---
+++ lib/http_manager.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class HttpManager
+{
+  static Future<http.Response> get({ String url, Map<String, String> headers}) async {
+    http.Response response;
+
+    try
+    {
+      response = await http.get(url, headers: headers);
+    }
+    on SocketException catch (e)
+    {
+      Get.snackbar(null, "Errore");
+    }
+
+    return response;
+  }
+}
