877f6162ee9015bd12fb10130ac82896d110fef9
Alex Sandri <me@alexsandri.com>
2020-09-30 20:32:11 +0200

    Update CalendarStrip

--- lib/widgets/ClasseVivaCalendarStrip.dart
+++ lib/widgets/ClasseVivaCalendarStrip.dart
@@ -18,22 +18,31 @@ class _ClasseVivaCalendarStripState extends State<ClasseVivaCalendarStrip> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _session.yearEndsAt.difference(_session.yearBeginsAt).inDays + 1,
-      itemBuilder: (context, index) {
-        final DateTime date = _session.yearBeginsAt.add(Duration(days: index));
+    return Container(
+      height: 80,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: _session.yearEndsAt.difference(_session.yearBeginsAt).inDays + 1,
+        itemBuilder: (context, index) {
+          final DateTime date = _session.yearBeginsAt.add(Duration(days: index));
 
-        return ListTile(
-          title: Text(date.day.toString()),
-          subtitle: Text(DateFormat.M().format(date)),
-          onTap: () {
-            // TODO
-            print(date);
+          return Container(
+            height: 80,
+            width: MediaQuery.of(context).size.width / 7, // 7 days in a row
+            child: ListTile(
+              title: Text(date.day.toString()),
+              subtitle: Text(DateFormat.M().format(date)),
+              onTap: () {
+                // TODO
+                print(date);
 
-            widget.onDateChange(date);
-          },
-        );
-      },
+                widget.onDateChange(date);
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
