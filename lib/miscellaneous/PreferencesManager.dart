cff9cca6448646d2c59b70ccb2c5c1b5b80158e2
Alex Sandri <me@alexsandri.com>
2020-09-30 14:48:48 +0200

    Fix: update return type

--- lib/miscellaneous/PreferencesManager.dart
+++ lib/miscellaneous/PreferencesManager.dart
@@ -4,9 +4,9 @@ class PreferencesManager
 {
   static Box _box = Hive.box("preferences");
 
-  static void get(String key) => _box.get(key);
+  static dynamic get(String key) => _box.get(key);
 
-  static void set(String key, dynamic value) => _box.put(key, value);
+  static Future<void> set(String key, dynamic value) => _box.put(key, value);
 
   static Future<void> initialize() async => _box = await Hive.openBox("preferences");
 }
