a4e5c54e51a7b991b91c0c6806b65e9ada16a74c
Alex Sandri <me@alexsandri.com>
2020-09-30 14:49:34 +0200

    Add delete method in PreferencesManager

--- lib/miscellaneous/PreferencesManager.dart
+++ lib/miscellaneous/PreferencesManager.dart
@@ -2,11 +2,13 @@ import 'package:hive/hive.dart';
 
 class PreferencesManager
 {
-  static Box _box = Hive.box("preferences");
+  static Box _box;
 
   static dynamic get(String key) => _box.get(key);
 
   static Future<void> set(String key, dynamic value) => _box.put(key, value);
+
+  static Future<void> delete(String key) => _box.delete(key);
 
   static Future<void> initialize() async => _box = await Hive.openBox("preferences");
 }
