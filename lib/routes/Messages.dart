8da1b62cc9279811df2856ca770743f281dfde6d
Alex Sandri <me@alexsandri.com>
2020-10-04 18:54:26 +0200

    Use ClasseVivaRefreshableView in Messages.dart

--- lib/routes/Messages.dart
+++ lib/routes/Messages.dart
@@ -1,103 +1,53 @@
 import 'package:classeviva_lite/miscellaneous/classeviva.dart';
 import 'package:classeviva_lite/models/ClasseVivaMessage.dart';
 import 'package:classeviva_lite/routes/Message.dart';
-import 'package:classeviva_lite/widgets/spinner.dart';
+import 'package:classeviva_lite/widgets/ClasseVivaRefreshableView.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-class Messages extends StatefulWidget {
-  @override
-  _MessagesState createState() => _MessagesState();
-}
-
-class _MessagesState extends State<Messages> {
-  final ClasseViva _session = ClasseViva.current;
-
-  List<ClasseVivaMessage> _messages;
-
-  Future<void> _handleRefresh() async {
-    await for (final List<ClasseVivaMessage> messages in _session.getMessages())
-    {
-      if (messages == null) continue;
-
-      if (mounted)
-        setState(() {
-          _messages = messages;
-        });
-    }
-  }
-
-  void initState() {
-    super.initState();
-
-    _handleRefresh();
-  }
-
+class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Messaggi"),
-        ),
-        body: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: RefreshIndicator(
-                  onRefresh: _handleRefresh,
-                  backgroundColor: Theme.of(context).appBarTheme.color,
-                  child: _messages == null
-                  ? Spinner()
-                  : ListView.separated(
-                      separatorBuilder: (context, index) => Divider(),
-                      itemCount: _messages.isNotEmpty
-                        ? _messages.length
-                        : 1,
-                      itemBuilder: (context, index) {
-                        if (_messages.isEmpty)
-                          return SelectableText(
-                            "Non sono presenti messaggi",
-                            textAlign: TextAlign.center,
-                          );
+    return ClasseVivaRefreshableView<List<ClasseVivaMessage>>(
+      title: "Messaggi",
+      stream: () => ClasseViva.current.getMessages(),
+      builder: (messages) {
+        return ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final ClasseVivaMessage message = messages[index];
 
-                        final ClasseVivaMessage message = _messages[index];
-
-                        return ListTile(
-                          title: Text(message.subject),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(DateFormat.yMMMMd().add_jms().format(message.createdAt)),
-                              Text(message.content),
-                            ],
-                          ),
-                          trailing: !message.isRead
-                            ? Tooltip(
-                                message: "Non letto",
-                                child: Icon(
-                                  Icons.circle,
-                                  color: ClasseViva.PRIMARY_LIGHT,
-                                ),
-                              )
-                            : null,
-                          onTap: () {
-                            message.markAsRead();
-
-                            Get.to(Message(message));
-                          },
-                        );
-                      },
-                    ),
-                )
+            return ListTile(
+              title: Text(message.subject),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(DateFormat.yMMMMd().add_jms().format(message.createdAt)),
+                  Text(message.content),
+                ],
               ),
-            ],
-          ),
-        ),
-      ),
+              trailing: !message.isRead
+                ? Tooltip(
+                    message: "Non letto",
+                    child: Icon(
+                      Icons.circle,
+                      color: ClasseViva.PRIMARY_LIGHT,
+                    ),
+                  )
+                : null,
+              onTap: () {
+                message.markAsRead();
+
+                Get.to(Message(message));
+              },
+            );
+          },
+        );
+      },
+      isResultEmpty: (result) => result.isEmpty,
+      emptyResultMessage: "Non sono presenti messaggi",
     );
   }
 }
