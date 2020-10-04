e4ddefbbfe54d1f1f23a6663aaf24b119cc01ad4
Alex Sandri <me@alexsandri.com>
2020-10-04 18:03:45 +0200

    Create ClasseVivaRefreshableView

---
+++ lib/widgets/ClasseVivaRefreshableView.dart
@@ -0,0 +1,66 @@
+import 'package:classeviva_lite/widgets/spinner.dart';
+import 'package:flutter/material.dart';
+
+class ClasseVivaRefreshableView<T> extends StatefulWidget {
+  final String title;
+
+  final Stream<T> Function() stream;
+
+  final Widget Function(T result) builder;
+
+  ClasseVivaRefreshableView({
+    @required this.title,
+    @required this.stream,
+    @required this.builder,
+  });
+
+  @override
+  _ClasseVivaRefreshableViewState<T> createState() => _ClasseVivaRefreshableViewState<T>();
+}
+
+class _ClasseVivaRefreshableViewState<T> extends State<ClasseVivaRefreshableView> {
+  T _result;
+
+  Future<void> _handleRefresh() async {
+    await for (final T result in widget.stream())
+    {
+      if (result == null) continue;
+
+      if (mounted) setState(() => _result = result);
+    }
+  }
+
+  void initState() {
+    super.initState();
+
+    _handleRefresh();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: _handleRefresh,
+                  backgroundColor: Theme.of(context).appBarTheme.color,
+                  child: _result == null
+                  ? Spinner()
+                  : widget.builder(_result),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
