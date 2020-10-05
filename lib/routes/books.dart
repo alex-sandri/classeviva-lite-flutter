4359b3e1e59d3cba6c44d4bf7a27623c37af66e3
Alex Sandri <me@alexsandri.com>
2020-10-05 17:35:00 +0200

    Rename book.dart in Book.dart

--- lib/routes/books.dart
+++ lib/routes/books.dart
@@ -1,6 +1,6 @@
 import 'package:classeviva_lite/miscellaneous/classeviva.dart';
 import 'package:classeviva_lite/models/ClasseVivaBook.dart';
-import 'package:classeviva_lite/routes/book.dart';
+import 'package:classeviva_lite/routes/Book.dart';
 import 'package:classeviva_lite/widgets/ClasseVivaRefreshableView.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
