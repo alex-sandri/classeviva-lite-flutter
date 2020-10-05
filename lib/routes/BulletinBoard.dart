7d0d64a27214046e3364ac540c5aaf40c5db063e
Alex Sandri <me@alexsandri.com>
2020-10-05 17:25:12 +0200

    Use ClasseVivaRefreshableView in BulletinBoard.dart

--- lib/routes/BulletinBoard.dart
+++ lib/routes/BulletinBoard.dart
@@ -2,7 +2,7 @@ import 'package:classeviva_lite/miscellaneous/ClasseVivaSearchDelegate.dart';
 import 'package:classeviva_lite/models/ClasseVivaBulletinBoardItem.dart';
 import 'package:classeviva_lite/routes/BulletinBoardItem.dart';
 import 'package:classeviva_lite/miscellaneous/classeviva.dart';
-import 'package:classeviva_lite/widgets/spinner.dart';
+import 'package:classeviva_lite/widgets/ClasseVivaRefreshableView.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
@@ -13,97 +13,42 @@ class BulletinBoard extends StatefulWidget {
 }
 
 class _BulletinBoardState extends State<BulletinBoard> {
-  final ClasseViva _session = ClasseViva.current;
-
-  List<ClasseVivaBulletinBoardItem> _items;
-
   bool _hideInactive = true;
-
-  Future<void> _handleRefresh() async {
-    await for (final List<ClasseVivaBulletinBoardItem> items in _session.getBulletinBoard(hideInactive: _hideInactive))
-    {
-      if (items == null) continue;
-
-      if (mounted)
-        setState(() {
-          _items = items;
-        });
-    }
-  }
-
-  void initState() {
-    super.initState();
-
-    _handleRefresh();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Bacheca"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: "Cerca",
-              onPressed: () => showSearch(
-                context: context,
-                delegate: ClasseVivaSearchDelegate(
-                  stream: (query) => ClasseViva.current.getBulletinBoard(
-                    query: query,
-                    hideInactive: false,
-                  ),
-                  builder: (item) => BulletinBoardListTile(item),
-                ),
+    return ClasseVivaRefreshableView<List<ClasseVivaBulletinBoardItem>>(
+      title: "Bacheca",
+      actions: [
+        IconButton(
+          icon: Icon(Icons.search),
+          tooltip: "Cerca",
+          onPressed: () => showSearch(
+            context: context,
+            delegate: ClasseVivaSearchDelegate(
+              stream: (query) => ClasseViva.current.getBulletinBoard(
+                query: query,
+                hideInactive: false,
               ),
-            )
-          ],
-        ),
-        body: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SwitchListTile(
-                title: Text("Nascondi comunicazioni non attive"),
-                value: _hideInactive,
-                onChanged: (checked) {
-                  setState(() {
-                    _items = null;
-
-                    _hideInactive = checked;
-                  });
-
-                  _handleRefresh();
-                },
-              ),
-              Expanded(
-                child: RefreshIndicator(
-                  onRefresh: _handleRefresh,
-                  backgroundColor: Theme.of(context).appBarTheme.color,
-                  child: _items.isNull
-                    ? Spinner()
-                    : ListView.builder(
-                        itemCount: _items.isNotEmpty
-                          ? _items.length
-                          : 1,
-                        itemBuilder: (context, index) {
-                          if (_items.isEmpty)
-                            return SelectableText(
-                              "Non sono presenti elementi in bacheca",
-                              textAlign: TextAlign.center,
-                            );
-
-                          return BulletinBoardListTile(_items[index]);
-                        },
-                      ),
-                ),
-              ),
-            ],
+              builder: (item) => BulletinBoardListTile(item),
+            ),
           ),
         ),
+      ],
+      head: SwitchListTile(
+        title: Text("Nascondi comunicazioni non attive"),
+        value: _hideInactive,
+        onChanged: (checked) => setState(() => _hideInactive = checked),
       ),
+      stream: () => ClasseViva.current.getBulletinBoard(hideInactive: _hideInactive),
+      builder: (items) {
+        return ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) =>  BulletinBoardListTile(items[index]),
+        );
+      },
+      isResultEmpty: (result) => result.isEmpty,
+      emptyResultMessage: "Non sono presenti elementi in bacheca",
     );
   }
 }
