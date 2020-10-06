41c5c3e10220a3d67687e69f4d00d3927f00ea30
Alex Sandri <me@alexsandri.com>
2020-10-06 17:11:29 +0200

    Use ClasseVivaRefreshableView in lessons.dart

--- lib/routes/lessons.dart
+++ lib/routes/lessons.dart
@@ -1,87 +1,32 @@
 import 'package:classeviva_lite/miscellaneous/classeviva.dart';
 import 'package:classeviva_lite/models/ClasseVivaLesson.dart';
 import 'package:classeviva_lite/models/ClasseVivaSubject.dart';
+import 'package:classeviva_lite/widgets/ClasseVivaRefreshableView.dart';
 import 'package:classeviva_lite/widgets/spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class Lessons extends StatefulWidget {
-  @override
-  _LessonsState createState() => _LessonsState();
-}
-
-class _LessonsState extends State<Lessons> {
-  final ClasseViva _session = ClasseViva.current;
-
-  List<ClasseVivaSubject> _subjects;
-
-  Future<void> _handleRefresh() async {
-    await for (final List<ClasseVivaSubject> subjects in _session.getSubjects())
-    {
-      if (subjects == null) continue;
-
-      if (mounted)
-        setState(() {
-          _subjects = subjects;
-        });
-    }
-  }
-
-  void initState() {
-    super.initState();
-
-    _handleRefresh();
-  }
-
+class Lessons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Lezioni'
-          ),
-        ),
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).requestFocus(FocusNode());
+    return ClasseVivaRefreshableView<List<ClasseVivaSubject>>(
+      title: "Lezioni",
+      stream: () => ClasseViva.current.getSubjects(),
+      builder: (subjects) {
+        return ListView.builder(
+          itemCount: subjects.length,
+          itemBuilder: (context, index) {
+            final ClasseVivaSubject subject = subjects[index];
+
+            return ExpansionTile(
+              title: Text(subject.name),
+              children: [ LessonsListView(subject: subject) ],
+            );
           },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: RefreshIndicator(
-                  onRefresh: _handleRefresh,
-                  backgroundColor: Theme.of(context).appBarTheme.color,
-                  child: _subjects == null
-                  ? Spinner()
-                  : ListView.builder(
-                      itemCount: _subjects.isNotEmpty
-                        ? _subjects.length
-                        : 1,
-                      itemBuilder: (context, index) {
-                        if (_subjects.isEmpty)
-                          return SelectableText(
-                            "Non sono presenti lezioni",
-                            textAlign: TextAlign.center,
-                          );
-
-                        final ClasseVivaSubject subject = _subjects[index];
-
-                        return ExpansionTile(
-                          title: Text(subject.name),
-                          children: [
-                            LessonsListView(subject: subject),
-                          ],
-                        );
-                      },
-                    ),
-                )
-              ),
-            ],
-          ),
-        ),
-      ),
+        );
+      },
+      isResultEmpty: (result) => result.isEmpty,
+      emptyResultMessage: "Non sono presenti lezioni",
     );
   }
 }
