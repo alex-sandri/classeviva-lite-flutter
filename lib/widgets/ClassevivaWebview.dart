46838ef8f187606f191f4a091fc056133224a346
Alex Sandri <me@alexsandri.com>
2020-10-07 18:41:48 +0200

    Remove unused import

--- lib/widgets/ClassevivaWebview.dart
+++ lib/widgets/ClassevivaWebview.dart
@@ -1,5 +1,4 @@
 import 'package:classeviva_lite/miscellaneous/classeviva.dart';
-import 'package:classeviva_lite/miscellaneous/theme_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
